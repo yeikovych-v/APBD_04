8a3731847953b202a13fd1190c1ac5f0291c4630
Volodymyr <yeikovychvolodymyrwork@gmail.com>
2024-03-27 12:21:48 +0100

    Removed unused birth date.

--- LegacyApp/UserCreditService.cs
+++ LegacyApp/UserCreditService.cs
@@ -28,7 +28,7 @@ namespace LegacyApp
         /// This method is simulating contact with remote service which is used to get info about someone's credit limit
         /// </summary>
         /// <returns>Client's credit limit</returns>
-        internal int GetCreditLimit(string lastName, DateTime dateOfBirth)
+        internal int GetCreditLimit(string lastName)
         {
             int randomWaitingTime = new Random().Next(3000);
             Thread.Sleep(randomWaitingTime);
